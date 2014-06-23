da710259d51fddda3f338d0c0fbefc373d0a2c90
Al Wold <alwold@alwold.com>
2014-06-23 00:23:13 -0700

    make tests actually do something

--- tests/check_merge.c
+++ tests/check_merge.c
@@ -1,4 +1,5 @@
 #include <check.h>
+#include <stdlib.h>
 #include "../src/merge.h"
 
 START_TEST (test_merge)
@@ -8,13 +9,27 @@ START_TEST (test_merge)
   ck_assert_int_eq(new_list[0], 1);
   ck_assert_int_eq(new_list[1], 2);
   ck_assert_int_eq(new_list[2], 3);
-  ck_assert_int_eq(new_list[3], 4);
-  ck_assert_int_eq(new_list[4], 5);
-  ck_assert_int_eq(new_list[5], 6);
+  ck_assert_int_eq(new_list[3], 5);
+  ck_assert_int_eq(new_list[4], 6);
+  ck_assert_int_eq(new_list[5], 7);
   free(new_list);
 }
 END_TEST
 
+Suite *merge_suite(void) {
+  Suite *s = suite_create("Merge Sort");
+  TCase *tc_core = tcase_create("Core");
+  tcase_add_test(tc_core, test_merge);
+  suite_add_tcase(s, tc_core);
+  return s;
+}
+
 int main(void) {
-  return 0;
+  int number_failed;
+  Suite *s = merge_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
