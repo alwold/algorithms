f37f47ec117a992b36872072c78b75864d961863
Al Wold <alwold@alwold.com>
2014-06-26 16:41:43 -0700

    add some test logic

--- src/linked_list_main.c
+++ src/linked_list_main.c
@@ -1,3 +1,10 @@
+#include "linked_list.h"
+#include "util.h"
+
 int main(int argc, char** argv) {
+  int data[] = {1, 2, 3, 4, 5, 6};
+  node *list = linked_list_create(data, 3);
+  list = linked_list_reverse(list);
+  print_linked_list(list);
   return 0;
 }
